d31e10d93a83d37560b4ea87962743314183717e
郭茜 <872122441@qq.com>
2017-02-27 16:15:52 +0800

    首页

---
+++ letian/first/view/HomeCell.h
@@ -0,0 +1,15 @@
+//
+//  HomeCell.h
+//  letian
+//
+//  Created by 郭茜 on 2017/2/27.
+//  Copyright © 2017年 J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeCell : UITableViewCell
+
++(instancetype) cellWithTableView:(UITableView *)tableView;
+
+@end
