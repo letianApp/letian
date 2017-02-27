2643621a185bd86e04bc7000e9d4542538197d44
郭茜 <872122441@qq.com>
2017-02-27 16:59:27 +0800

    首页

--- letian/first/controller/FirstViewController.m
+++ letian/first/controller/FirstViewController.m
@@ -8,7 +8,14 @@
 
 #import "FirstViewController.h"
 #import "YYCycleScrollView.h"
-@interface FirstViewController ()
+#import "HomeCell.h"
+
+@interface FirstViewController ()<UISearchBarDelegate,UITableViewDataSource,UITableViewDelegate>
+
+@property (nonatomic,strong) UITableView *tableView;
+
+@property(nonatomic,strong)UIView *headBgView;
+
 
 @end
 
@@ -16,35 +23,93 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-
-    [self customTabBar];
     
     self.navigationController.navigationBarHidden=YES;
     
-    [self createScrollView];
+    [self customTabBar];
+    
+    [self createHeadBgView];
+    
+    [self createTableView];
     
     
-
 }
+
+
 
 #pragma mark 定制TabBar
 - (void)customTabBar {
     
     self.tabBarItem = [[UITabBarItem alloc]initWithTitle:@"首页" image:[[UIImage imageNamed:@"firstPageTab"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal] selectedImage:[[UIImage imageNamed:@"firstPageTabSel"]imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]];
-    self.tabBarController.tabBar.tintColor = MAINCOLOR;
+    self.tabBarController.tabBar.tintColor = [UIColor blackColor];
     
 }
 
+
+-(void)createTableView
+{
+    UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_W, SCREEN_H-44) style:UITableViewStylePlain];
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    tableView.separatorStyle=UITableViewCellSeparatorStyleNone;
+    [self.view addSubview:tableView];
+    self.tableView = tableView;
+    self.tableView.tableHeaderView=self.headBgView;
+    
+}
+
+//头视图
+-(void)createHeadBgView
+{
+    self.headBgView=[[UIView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_W, 420)];
+    
+    UIView *view=[[UIView alloc]initWithFrame:CGRectMake(0, 220, SCREEN_W, 200)];
+    
+    NSArray *nameArray=@[@"- 咨询 -",@"- 文章 -",@"- 测试 -",@"- 活动 -"];
+    for (NSInteger i=0; i<2; i++) {
+        for (NSInteger j=0; j<2; j++) {
+            UILabel *label=[[UILabel alloc]initWithFrame:CGRectMake(SCREEN_W/2*j, i*100, SCREEN_W/2, 100)];
+            label.backgroundColor=[UIColor whiteColor];
+            label.text=nameArray[j+i*2];
+            label.textColor=[UIColor blackColor];
+            label.textAlignment=NSTextAlignmentCenter;
+            
+            label.userInteractionEnabled=YES;
+            UITapGestureRecognizer *tap=[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(labelClicked:)];
+            [label addGestureRecognizer:tap];
+            
+            [view addSubview:label];
+        }
+    }
+    
+    
+    
+    
+    
+    [self.headBgView addSubview:[self createScrollView]];
+    
+    [self.headBgView addSubview:view];
+    
+    [self createSearchBarOnView:self.headBgView];
+    
+    [self createmsgBtnOnView:self.headBgView];
+    
+}
+
+-(void)labelClicked:(UILabel *)label
+{
+    NSLog(@"咨询页面");
+    
+}
 #pragma mark 定制首页轮播图
--(void)createScrollView
+-(YYCycleScrollView *)createScrollView
 {
     
-    YYCycleScrollView *cycleScrollView = [[YYCycleScrollView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_W, 250) animationDuration:4.0];
+    YYCycleScrollView *cycleScrollView = [[YYCycleScrollView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_W, 220) animationDuration:4.0];
     
     NSMutableArray *viewArray = [[NSMutableArray alloc] init];
     for (int i = 0; i < 4; i++) {
-        UIImageView *tempImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_W, 250)];
+        UIImageView *tempImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_W, 220)];
         
         
         tempImageView.image = [UIImage imageNamed:[NSString stringWithFormat:@"index_slide_0%d.jpg",i+1]];
@@ -64,12 +129,110 @@
         
     }];
     
-    [self.view addSubview:cycleScrollView];
+    
+    
+    
+    return cycleScrollView;
+    
+}
 
+
+
+#pragma mark 搜索栏
+-(void)createSearchBarOnView:(UIView *)bgView
+{
+    UISearchBar * searchbar = [[UISearchBar alloc]initWithFrame:CGRectMake(100, 20, SCREEN_W-160, 40)];
+    searchbar.delegate=self;
+    searchbar.barStyle=UISearchBarStyleDefault;
+    searchbar.searchBarStyle=UISearchBarStyleDefault;
+    searchbar.placeholder=@"请输入关键字";
+    [bgView addSubview:searchbar];
+    
+    UIImage* searchBarBg = [self GetImageWithColor:[UIColor clearColor] andHeight:32.0f];
+    [searchbar setBackgroundImage:searchBarBg];
     
     
 }
 
+
+#pragma mark ----创建消息按钮
+-(void)createmsgBtnOnView:(UIView *)bgView
+{
+    
+    UIButton *msgBtn = [[UIButton alloc]initWithFrame:CGRectMake(SCREEN_W-50, 25, 30, 30)];
+    [msgBtn setImage:[UIImage imageNamed:@"msg"] forState:UIControlStateNormal];
+    [msgBtn addTarget:self action:@selector(msgBtnClick) forControlEvents:UIControlEventTouchUpInside];
+    [bgView addSubview:msgBtn];
+    
+}
+
+
+//消息按钮点击
+-(void)msgBtnClick
+{
+    NSLog(@"进入消息按钮");
+    
+}
+
+
+
+#pragma mark -----------------tableViewDelegate
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+//cell定制
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    HomeCell *cell=[HomeCell cellWithTableView:tableView];
+    
+    return cell;
+}
+
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 80;
+}
+
+
+#pragma mark -----------------searchBarDelegate
+//即将开始编辑
+- (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar{
+    
+    searchBar.showsCancelButton=YES;
+}
+
+
+//点击返回按钮
+-(void)searchBarCancelButtonClicked:(UISearchBar *)searchBar
+{
+    [searchBar resignFirstResponder];
+    searchBar.showsCancelButton=NO;
+}
+
+
+//点击搜索
+-(void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
+{
+    [searchBar resignFirstResponder];
+    searchBar.showsCancelButton=NO;
+}
+
+
+//实现搜索条背景透明化
+- (UIImage*) GetImageWithColor:(UIColor*)color andHeight:(CGFloat)height{
+    CGRect r= CGRectMake(0.0f, 0.0f, 1.0f, height);
+    UIGraphicsBeginImageContext(r.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetFillColorWithColor(context, [color CGColor]);
+    CGContextFillRect(context, r);
+    UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return img;
+}
 
 
 
@@ -79,13 +242,13 @@
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
---
+++ letian/first/view/HomeCell.m
@@ -0,0 +1,42 @@
+//
+//  HomeCell.m
+//  letian
+//
+//  Created by 郭茜 on 2017/2/27.
+//  Copyright © 2017年 J. All rights reserved.
+//
+
+#import "HomeCell.h"
+
+@implementation HomeCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+
++(instancetype) cellWithTableView:(UITableView *)tableView
+{
+    HomeCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    if (cell == nil) {
+        cell = [[self alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+    }
+    return cell;
+}
+
+-(instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        self = [[NSBundle mainBundle] loadNibNamed:NSStringFromClass([self class]) owner:nil options:nil].firstObject;
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
