29a4fbf2e99f23b9414035bef861892f06e97d6a
郭茜 <872122441@qq.com>
2017-03-09 11:53:36 +0800

    真机调试证书，拨打客服电话

---
+++ letian/my/view/MessageCell.h
@@ -0,0 +1,15 @@
+//
+//  MessageCell.h
+//  letian
+//
+//  Created by 郭茜 on 2017/3/9.
+//  Copyright © 2017年 J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessageCell : UITableViewCell
+
++(instancetype) cellWithTableView:(UITableView *)tableView;
+
+@end
